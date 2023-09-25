52daad74b90fb01bf0c88fd0081b4b2f13508726
Daniel Jeznach <djeznach@gmail.com>
2023-09-25 17:27:58 +0200

    Add mode to slide all subtitles forward or backwards

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SRTedit
 
-Simple SRT subtitles editor. It allows manipulation of subtitles duration, for people who read to slow to follow fast-paced subtitles :)
+Simple SRT subtitles editor. It allows manipulation of subtitles duration, for people who read too slow to follow fast-paced subtitles :)
+It can also move all subtitles forwards or backwards by given interval.
--- srtedit.py
+++ srtedit.py
@@ -9,10 +9,12 @@ import sys
 def parse_args(args):
     parser = argparse.ArgumentParser()
     mode = parser.add_mutually_exclusive_group(required=True)
-    mode.add_argument('-i', '--interval', type=float, default=1, metavar='duration',
+    mode.add_argument('-i', '--interval', type=float, metavar='duration',
                       help='Amount of seconds to add. Accepts negative numbers.')
-    mode.add_argument('-m', '--minimum', type=float, default=1, metavar='duration',
+    mode.add_argument('-m', '--minimum', type=float, metavar='duration',
                       help='Minimum subtitle display time. Shorter subtitles will be expanded to this value.')
+    mode.add_argument('-s', '--slide', type=float, metavar='duration',
+                      help='Display all subtitles later (positive) or earlier (negative value) by duration')
     parser.add_argument('-e', '--encoding', help='File encoding', default='cp1250', metavar='encoding')
     parser.add_argument('-f', '--file', help='SRT file to process', required=True, metavar='filename')
     parser.add_argument('-o', '--output', default=None, metavar='filename',
@@ -36,8 +38,10 @@ def main(args):
 
     if args.minimum:
         delta = datetime.timedelta(seconds=args.minimum)
-    else:
+    elif args.interval:
         delta = datetime.timedelta(seconds=args.interval)
+    else:
+        delta = datetime.timedelta(seconds=args.slide)
 
     for line in data:
         result = pattern.search(line)
@@ -55,11 +59,18 @@ def main(args):
                     new_end = ts_start + delta
                 else:
                     new_end = ts_end
+                new_start = ts_start
+            elif args.interval:
+                new_start = ts_start
+                new_end = ts_end + delta
             else:
+                new_start = ts_start + delta
                 new_end = ts_end + delta
 
+            new_start_time = new_start.strftime('%H:%M:%S,%f')[0:-3]
             new_end_time = new_end.strftime('%H:%M:%S,%f')[0:-3]
-            line = f'{start_time} --> {new_end_time}'
+
+            line = f'{new_start_time} --> {new_end_time}'
 
         new_data.append(line)
 
